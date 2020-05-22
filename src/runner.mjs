7fb3e97059ce138b1af6bd6d0ef4243462e11eaf
oikumo <oikumo@gmail.com>
2020-05-22 00:52:32 -0400

    Updated version and Refactor

--- src/runner.mjs
+++ src/runner.mjs
@@ -2,14 +2,14 @@ import * as fs from 'fs'
 import path from 'path'
 import { results } from './test.mjs'
 
-export const runTests = function (testFolder) {
+export const runTests = function (testPath) {
     results.len = 0
 
-    const dirs = fs.readdirSync(testFolder)
+    const dirs = fs.readdirSync(testPath)
     const promises = []
 
     dirs.forEach((dir) => {
-        const file = path.join(testFolder, dir)
+        const file = path.join(testPath, dir)
         promises.push(import(file))
     })
 
