877fb358130507f6a1a87ba5edf6f4b54faa3224
oikumo <oikumo@gmail.com>
2021-04-04 17:09:54 -0400

    Refactor index

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
 import { test } from './src/test.js';
 import { runTests } from './src/runner.js';
-import * as arrays from './src/assertions/assert-arrays.js';
-import * as equality from './src/assertions/assert-equality.js';
-import * as errors from './src/assertions/assert-errors.js';
-import * as nulls from './src/assertions/assert-nulls.js';
-
-const assert = Object.assign({},
-    arrays,
-    equality,
-    errors,
-    nulls
-);
+import * as assert from './src/assert.js';
 
 export {
     test,
